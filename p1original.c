58c0ca39269a6c6b3e501d07538f46f161b086f7
BHARATH2145 <94664858+Bharath2145@users.noreply.github.com>
2022-02-07 07:22:31 +0000

    not done yet

--- p1original.c
+++ p1original.c
@@ -1,2 +1,25 @@
+#include<stdio.h>
+void input(float base, float height)
+{
+  float a, b;
+  printf("enter the base and height of the triangle\n");
+  scanf("%f%f",&a, &b);
+}
+float area(float a, float b, float area);
+{
+  area=0.5*(a*b);
+  return area;
+}
+ float output(float a, float b,float area);  
+ printf("area of the trainagle %f is %f", a, b, area);
+ return 0;
+}
+int main()
+{
+  float a, b, area;
+  a=input;
+  b=input;
+  
 
 }
+ 
