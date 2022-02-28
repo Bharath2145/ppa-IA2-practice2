b4bd3905d25206dee9fb8ba579e36dbd9433f100
BHARATH2145 <94664858+Bharath2145@users.noreply.github.com>
2022-02-28 06:46:37 +0000

    .

--- p8original.c
+++ p8original.c
@@ -3,7 +3,7 @@ struct _triangle
 {
   float base,altitude,area;
 };
-typedef struct _triangle triangle;
+typedef struct _triangle Triangle;
 int input_n()
 {
   int n;
@@ -11,7 +11,7 @@ int input_n()
   scanf("%d", &n);
   return n;  
 }
-void input_n_triangles(int n, triangle t[n])
+void input_n_triangles(int n, Triangle t[n])
 {
  int i;
  for(i=0;i<n;i++)
@@ -21,33 +21,38 @@ void input_n_triangles(int n, triangle t[n])
     scanf("%f %f",&t[i].base,&t[i].altitude);
   }
 }
-void find_area_n(int n,trianglet[n])
+void find_area_n(int n,Triangle t[n])
 {
   int i;
   t[i].area=0;
   for(i=0;i<n;i++)
     {
-      t[i].area=((t[i].base)*(t[i].altidue))/2;
-      printf("area of triangle is %f\n",t[i].area);
+      t[i].area=((t[i].base)*(t[i].altitude))/2;
+      printf("area of triangle is %.1f\n",t[i].area);
     }
 }
 Triangle find_smallest_triangle(int n, Triangle t[n])
 {
-  for(i=0;i<n;i++)
-    {
-      if(t[i])
-    }
-    
+  for(int i=0;i<n;i++)
+  {
+    if(t[i].area<t[i+1].area)
+    return t[i].area;
+    else
+    return t[i+1].area;
+  }   
 }
-void output(int n,triangle t[n], triangle smallest)
+void output(int n,Triangle t[n], Triangle smallest)
 {
-  printf("the triangle with base %f and altitude %f has the smallest of all\n",smallest.base, smallest.altitude);
+  printf("the triangle with base %.1f and altitude %.1f has the smallest area of all\n",smallest.base, smallest.altitude);
 }
 int main()
 {
-  triangle t;
-  t=input_triangle();
-  find_area(&t);
-  output(t);
+  int i, n;
+  Triangle t[20].smallest;
+  n= input_n();
+  input_n_triangle(n,t);
+  find_area_n(n,t);
+  smallest = find_smallest_triangle(n,t);
+  output(n,t,smallest);
   return 0;
 }
